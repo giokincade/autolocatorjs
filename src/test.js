b15f2d35f6c61c2701b415c31859bab708a68391
Giovanni Fernandez-Kincade <giokincade@gmail.com>
2020-03-09 11:24:54 -0400

    web server works

--- src/test.js
+++ src/test.js
@@ -1,15 +1,38 @@
+const wifi = require("Wifi");
+const http = require("http");
+
 const WIFI_NAME = "FiOS-52W4I";
 const WIFI_OPTIONS = { password : "fund9388ion9279net" };
+const connectToWifi = () => {
+	wifi.connect(WIFI_NAME, WIFI_OPTIONS, (err)  => {
+		if (err) {
+		  console.log("Connection error: " + err);
+		  return;
+		} else {
+			console.log("Wifi Connected!");
+			wifi.getIP((err, info) => {
+				if (info && info.ip) {
+					console.log("IP Address: " + info.ip );
+                    startServer();
+				}
+			});
+		}
+	});
+};
+
+const onHttpRequest = (request, response) => {
+    console.log("Processing request");
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write("<html><body>Boom</body></html>");
+    response.end("</body></html>");
+};
+
+const startServer = () => {
+    console.log("Starting Server");
+    http.createServer(onHttpRequest).listen(80);
+};
 
 function onInit() {
-	const wifi = require("Wifi");
-	wifi.connect(WIFI_NAME, WIFI_OPTIONS, (err)  => {
-	if (err) {
-	  console.log("Connection error: " + err);
-	  return;
-	} else {
-		console.log("Wifi Connected!");
-	}
-	});
+	connectToWifi();
 }
 onInit();
