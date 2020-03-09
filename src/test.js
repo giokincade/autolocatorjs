2bf3b137ac93d8d57dfcae1f64a04da3ce7ca446
Giovanni Fernandez-Kincade <giokincade@gmail.com>
2020-03-09 10:57:06 -0400

    on init

--- src/test.js
+++ src/test.js
@@ -1,21 +1,15 @@
-var WIFI_NAME = "FiOS-52W4I";
-var WIFI_OPTIONS = { password : "fund9388ion9279net" };
+const WIFI_NAME = "FiOS-52W4I";
+const WIFI_OPTIONS = { password : "fund9388ion9279net" };
 
-var wifi = require("Wifi");
-wifi.connect(WIFI_NAME, WIFI_OPTIONS, function(err) {
-  if (err) {
-    console.log("Connection error: "+err);
-    return;
-  }
-  console.log("Connected!");
-  getPage();
-});
-
-function getPage() {
-  require("http").get("http://www.pur3.co.uk/hello.txt", function(res) {
-    console.log("Response: ",res);
-    res.on('data', function(d) {
-      console.log("--->"+d);
-    });
-  });
+function onInit() {
+	const wifi = require("Wifi");
+	wifi.connect(WIFI_NAME, WIFI_OPTIONS, (err)  => {
+	if (err) {
+	  console.log("Connection error: " + err);
+	  return;
+	} else {
+		console.log("Wifi Connected!");
+	}
+	});
 }
+onInit();
