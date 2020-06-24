48e1d86d58255a60b9f290eeb822e8e71cf9328e
Matt Walsh <mattwalsh@Matts-MacBook-Pro.local>
2020-06-24 12:20:03 -0400

    added mw test

---
+++ src/mw-test.js
@@ -0,0 +1,53 @@
+const wifi = require("Wifi");
+const ws = require("ws");
+
+const WIFI_NAME = "MeowNetJr";
+const WIFI_OPTIONS = { password : "cookiejr" };
+const connectToWifi = () => {
+	wifi.connect(WIFI_NAME, WIFI_OPTIONS, (err)  => {
+		if (err) {
+		  console.log("Connection error: " + err);
+		  return;
+		} else {
+			console.log("Wifi Connected!");
+			
+			var info = {
+				ip: "192.168.1.9"
+			};
+			
+			wifi.setIP(info, wifi.getIP((err) => {}));
+		}
+	});
+};
+
+var page = '<html><body><script>var ws;setTimeout(function(){';
+page += 'const ws = new WebSocket("ws://" + location.host);';
+page += 'ws.onmessage = function (event) { console.log("MSG:"+event.data); };';
+page += 'setTimeout(function() { ws.send("Hello to Espruino!"); }, 1000);';
+page += '</script><p>something</p></body></html>';
+
+const onHttpRequest = (request, response) => {
+	console.log("Processing request");
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.end(page);
+};
+
+const startServer = () => {
+	console.log("Starting Server");
+	ws
+		.createServer(onHttpRequest)
+		.listen(80)
+		.on("websocket", (socket) => {
+			console.log("websocket connection");
+			socket.on("message", (msg) => {
+				console.log("websocket message: " + msg);
+				socket.send("pong");
+			})
+		});
+
+};
+
+function onInit() {
+	connectToWifi();
+}
+onInit();
