eea1c63fada2b963658afa54fc5af66b2aa234d6
blive2 <blive2@Media1s-Mac-Pro.local>
2020-08-07 14:28:23 -0400

    working on tests

--- src/test.js
+++ src/test.js
@@ -1,49 +1,53 @@
 const wifi = require("Wifi");
 const ws = require("ws");
 
-const WIFI_NAME = "MeowNetJr";
-const WIFI_OPTIONS = { password : "cookiejr" };
+const WIFI_NAME = "OceanusStudio";
+const WIFI_OPTIONS = { password : "studio125" };
+
 const connectToWifi = () => {
+	wifi.reset();
+
 	wifi.connect(WIFI_NAME, WIFI_OPTIONS, (err)  => {
 		if (err) {
 		  console.log("Connection error: " + err);
 		  return;
 		} else {
 			console.log("Wifi Connected!");
-			wifi.getIP((err, info) => {
-				if (info && info.ip) {
-					console.log("IP Address: " + info.ip );
-                    startServer();
-				}
+
+			var info = {
+					ip: "192.168.0.159",
+					gw: "192.168.0.1",
+					netmask: "255.255.255.0"
+			};
+
+			wifi.setIP(info, (err) => {
+					console.log("IP set!");
+					startServer();
 			});
 		}
 	});
 };
 
-var page = '<html><body><script>var ws;setTimeout(function(){';
-page += 'const ws = new WebSocket("ws://" + location.host);';
-page += 'ws.onmessage = function (event) { console.log("MSG:"+event.data); };';
-page += 'setTimeout(function() { ws.send("Hello to Espruino!"); }, 1000);';
-page += '</script><p>something</p></body></html>';
+var page = 'allo guv';
 
 const onHttpRequest = (request, response) => {
-    console.log("Processing request");
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(page);
+	console.log("Processing request");
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.end(page);
 };
 
 const startServer = () => {
-    console.log("Starting Server");
-    ws
-        .createServer(onHttpRequest)
-        .listen(80)
-        .on("websocket", (socket) => {
-            console.log("websocket connection");
-            socket.on("message", (msg) => {
-                console.log("websocket message: " + msg);
-                socket.send("pong");
-            })
-        });
+	console.log("Starting Server");
+	ws
+		.createServer(onHttpRequest)
+		.listen(80)
+		.on("websocket", (socket) => {
+			console.log("websocket connection");
+			socket.on("message", (msg) => {
+				console.log("websocket message: " + msg);
+				socket.send("pong");
+			});
+		});
 
 };
 
