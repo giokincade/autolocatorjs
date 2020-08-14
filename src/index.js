f1f78b026b02b44a07d9efbdca578d1f83ab04ee
Giovanni Fernandez-Kincade <giokincade@gmail.com>
2020-08-13 22:11:33 -0400

    omg its working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,114 @@
-const ws = new WebSocket("ws://" + location.host);'
-ws.onmessage = function (event) { console.log("MSG:"+event.data); };
-setTimeout(function() { ws.send("Hello to Espruino!"); }, 1000);
+const wifi = require("Wifi");
+const ws = require("ws");
+
+const PINS = {
+    TACH: A0,
+    TACH_DIRECTION: A1,
+    PLAY: A4
+};
+const WIFI_NAME = "OceanusStudio";
+const WIFI_OPTIONS = { password : "studio125" };
+const DEBUG = true;
+
+const log = (msg) => {
+    if(DEBUG) {
+        console.log(msg);
+	}
+};
+
+
+pinMode(
+  PINS.TACH,
+  'input_pullup'
+);
+pinMode(
+  PINS.TACH_DIRECTION,
+  'input_pullup'
+);
+
+const tachWatcher = (function(){
+  var bin=atob("AUt7RBhocEdGAAAAAkt7RIDwAQAYcHBHNgAAAAdLe0QbeCOxBkp6RBNoATMD4AVKekQTaAE7E2BwRwC/JgAAACIAAAAYAAAAAQ==");
+  return {
+    handlePulse:E.nativeCall(29, "void()", bin),
+    getTach:E.nativeCall(1, "int()", bin),
+    handleDirection:E.nativeCall(13, "void()", bin),
+  };
+})();
+
+const getTime = () => {
+  const secondsRaw = tachWatcher.getTach() / 4800.0;
+  const minutes = Math.floor(secondsRaw / 60);
+  const seconds = secondsRaw - minutes * 60.0;
+  return minutes + ":" + seconds;
+};
+
+
+const attachInterupts = () => {
+  setWatch(
+    tachWatcher.handlePulse,
+    PINS.TACH,
+    {repeat:true, edge:"rising", irq:true}
+  );
+
+  setWatch(
+    tachWatcher.handleDirection,
+    PINS.TACH_DIRECTION,
+    {repeat:true, edge:"both", irq:true}
+  );
+};
+
+setInterval(
+    () => {
+        log("Tach = " + tachWatcher.getTach());
+        log("Time = " + getTime());
+    },
+    1000
+);
+const connectToWifi = () => {
+	wifi.connect(WIFI_NAME, WIFI_OPTIONS, (err)  => {
+		if (err) {
+		  log("Connection error: " + err);
+		  return;
+		} else {
+			log("Wifi Connected!");
+
+			var info = {
+					ip: "192.168.1.200",
+                    gw: "192.168.0.1",
+                    netmask: "255.255.255.0"
+			};
+
+			wifi.setIP(info, (err) => {
+					log("IP set!");
+					startServer();
+			});
+		}
+	});
+};
+
+const onHttpRequest = (request, response) => {
+	log("Processing request");
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.end(getTime());
+};
+
+const startServer = () => {
+	log("Starting Server");
+	ws
+		.createServer(onHttpRequest)
+		.listen(80)
+		.on("websocket", (socket) => {
+			log("websocket connection");
+			socket.on("message", (msg) => {
+				log("websocket message: " + msg);
+				socket.send("pong");
+			});
+		});
+
+};
+
+function onInit() {
+	connectToWifi();
+    attachInterupts();
+}
+
