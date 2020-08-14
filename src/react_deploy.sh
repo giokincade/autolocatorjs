ab87cbaeb84f6511997d0960585f994add6e52e0
blive2 <blive2@Media1s-Mac-Pro.local>
2020-08-14 14:42:39 -0400

    public read

--- src/react_deploy.sh
+++ src/react_deploy.sh
@@ -3,4 +3,4 @@ source ./config.txt
 aws configure set aws_access_key_id $aws_key
 aws configure set aws_secret_access_key $aws_secret
 aws configure set default.region us-east-1
-aws s3 cp ./test.js s3://autolocator/test.js
+aws s3 cp ./test.js s3://autolocator/test.js --acl public-read
